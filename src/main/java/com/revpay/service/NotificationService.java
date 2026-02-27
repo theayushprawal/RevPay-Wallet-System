7dd47daa7b2d4b19b46897d129ea83e38be12255
Ayush Prawal <ayushprawal@gmail.com>
2026-02-27 22:06:37 +0530

    Implemented notification logic and integrated it to money request

---
+++ src/main/java/com/revpay/service/NotificationService.java
@@ -0,0 +1,27 @@
+package com.revpay.service;
+
+import java.util.List;
+
+import com.revpay.model.Notification;
+import com.revpay.model.User;
+import com.revpay.model.enums.NotificationType;
+
+public interface NotificationService {
+
+    /**
+     * Create a notification for a user
+     */
+    void createNotification(User user,
+                            String message,
+                            NotificationType type);
+
+    /**
+     * Get all notifications for a user
+     */
+    List<Notification> getNotificationsForUser(Long userId);
+
+    /**
+     * Mark a notification as read
+     */
+    void markAsRead(Long notificationId);
+}
